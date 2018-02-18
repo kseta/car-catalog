7187459902a117a59b88d00bcb3dc2d8b2273213
kseta <keigo.seta@gmail.com>
2018-02-19 02:46:10 +0900

    add twig module

---
+++ src/Module/HtmlModule.php
@@ -0,0 +1,13 @@
+<?php
+namespace MyVendor\MyProject\Module;
+
+use Madapaja\TwigModule\TwigModule;
+use Ray\Di\AbstractModule;
+
+class HtmlModule extends AbstractModule
+{
+    protected function configure()
+    {
+        $this->install(new TwigModule);
+    }
+}
